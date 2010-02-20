435a993a210e909696089d36fe7250090b46bcc3
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2010-02-20 20:08:27 +0900

    Add msp430-toolchain.sh meta script

--- msp430-toolchain.sh
+++ msp430-toolchain.sh
@@ -4,9 +4,7 @@
 
 PATH=$prefix/bin:$PATH
 
-$scriptdir/msp430-toolchain.sh
-
-modules="nesc tinyos-tools"
+modules="binutils gcc msp430-libc gdb gdbproxy mspdebug"
 
 for module in $modules; do
     $scriptdir/$module.sh download build install cleanup
