7751fa98d2cc972b9382230e55aaed0d5b6cddea
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2010-02-20 20:01:33 +0900

    Add gdbproxy to tinyos-msp430-toolchain.sh

--- tinyos-msp430-toolchain.sh
+++ tinyos-msp430-toolchain.sh
@@ -2,7 +2,9 @@
 
 . $(dirname $0)/main.subr
 
-modules="nesc binutils gcc msp430-libc tinyos-tools mspdebug gdb"
+PATH=$prefix/bin:$PATH
+
+modules="binutils gcc msp430-libc gdb gdbproxy mspdebug nesc tinyos-tools"
 
 for module in $modules; do
     $scriptdir/$module.sh download prepare build install cleanup
