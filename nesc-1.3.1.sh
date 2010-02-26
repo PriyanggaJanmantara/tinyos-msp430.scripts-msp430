19c45f647ce735a83edcdeb1ae88840268a6cf55
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2010-02-27 05:32:06 +0900

    fix undefined var osx10

--- nesc-1.3.1.sh
+++ nesc-1.3.1.sh
@@ -5,7 +5,7 @@ prefix=/stow
 urlnesc=http://softlayer.dl.sourceforge.net/project/nescc/nescc/v1.3.1
 builddir=nesc-1.3.1
 
-expr $(uname -r) : 10.0 > /dev/null && osx10=yes
+expr $(uname -r) : 10.0 > /dev/null && osx10=yes || osx=no
 
 function die() {
     echo "$@" 1>&2
@@ -18,7 +18,7 @@ function die() {
 
 rm -rf $builddir
 tar xzf nesc-1.3.1.tar.gz
-[ -n "$osx10" ] \
+[ "$osx10" = yes ] \
     && patch -d $builddir -p1 < $scriptdir/nesc-1.3.1-osx_10.patch \
     || die "apply patch failed"
 
