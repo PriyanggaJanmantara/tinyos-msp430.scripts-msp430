61b13d9df87090252e88a14bb7c36ec365f60267
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2010-02-27 05:30:25 +0900

    use tmp shell var

--- nesc-1.3.1.sh
+++ nesc-1.3.1.sh
@@ -4,12 +4,13 @@ scriptdir=$(dirname $0)
 prefix=/stow
 urlbase=http://softlayer.dl.sourceforge.net/project/nescc/nescc/v1.3.1
 builddir=nesc-1.3.1
+expr $(uname -r) : 10.0 > /dev/null && osx10=yes
 
 [ -f nesc-1.3.1.tar.gz ] || curl -O $urlbase/nesc-1.3.1.tar.gz
 
 rm -rf $builddir
 tar xzf nesc-1.3.1.tar.gz
-expr $(uname -r) : 10.0 >/dev/null && \
+[ -n "$osx10" ] && \
     patch -d $builddir -p1 < $scriptdir/nesc-1.3.1-osx_10.patch
 
 cd $builddir
