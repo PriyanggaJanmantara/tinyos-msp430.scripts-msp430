ba0c5790ee973f96cc6ac74f9ff0c0b2eba5c698
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2010-02-27 05:23:26 +0900

    reuse tinyos-2.x in parent directory

--- tinyos-2.x-tools.sh
+++ tinyos-2.x-tools.sh
@@ -5,15 +5,18 @@ scriptdir=$(dirname $0)
 
 builddir=build-tinyos-tools
 
-[[ -d $tinyos ]] \
-    && { cd $tinyos; cvs -q up; cd ..; } \
-    || { \
-        cvs -q -d $repotinyos co -P $tinyos \
-        || die "can not fetch from cvs repository"; \
-    }
-
 rm -rf $builddir
-cp -R $tinyos/tools $builddir
+if [[ -d $tinyos ]]; then
+    cd $tinyos; cvs -q up; cd ..;
+    cp -R $tinyos/tools $builddir
+elif [[ -d ../$tinyos ]]; then
+    cp -R ../$tinyos/tools $builddir
+else
+    cvs -q -d $repotinyos co -P $tinyos \
+        || die "can not fetch from cvs repository"; \
+	cp -R $tinyos/tools $builddir
+fi
+
 cd $builddir
 ./Bootstrap \
     || die "bootstrap failed"
