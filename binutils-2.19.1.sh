8ce2a4cc41b705ab6931ae4c7e37ba2fff75159a
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2010-02-27 05:30:40 +0900

    fix binutils to be compiled under 10.0

--- binutils-2.19.1.sh
+++ binutils-2.19.1.sh
@@ -6,6 +6,7 @@ binutils=binutils-2.19.1
 repourl=https://mspgcc4.svn.sourceforge.net/svnroot/mspgcc4
 urlbase=ftp://ftp.gnu.org/pub/gnu/binutils
 builddir=build-binutils
+expr $(uname -r) : 10.0 > /dev/null && osx10=yes
 
 function die() {
     echo "$@" 1>&2
@@ -24,6 +25,7 @@ patch -p1 -d $binutils < $scriptdir/$binutils-dollar.patch
 rm -rf $builddir
 mkdir -p $builddir
 cd $builddir
-../$binutils/configure --target=msp430 --prefix=$prefix --disable-nls
+[ -n "$osx10" ] && disable_werror=--disable-werror
+../$binutils/configure --target=msp430 --prefix=$prefix --disable-nls $disable_werror
 make -j32
 # sudo make install
