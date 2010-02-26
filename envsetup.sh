79c6d8fad02210d0c004e17cf18c2c94a010d55d
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2010-02-27 05:19:10 +0900

    add path setting.

--- envsetup.sh
+++ envsetup.sh
@@ -2,16 +2,22 @@
 # installation by substituting @prefix@ for the actual tinyos tree
 # installation point.
 
+. $HOME/.bash_library/path.sh
+
 export TOSROOT=
 export TOSDIR=
 export MAKERULES=
 
-TOSROOT="/opt/tinyos/tinyos-2.x"
-TOSDIR="$TOSROOT/tos"
-CLASSPATH=$CLASSPATH:$TOSROOT/support/sdk/java
-MAKERULES="$TOSROOT/support/make/Makerules"
+TOSROOT=/opt/tinyos/tinyos-2.x
+TOSDIR=$TOSROOT/tos
+MAKERULES=$TOSROOT/support/make/Makerules
+path::addpath $TOSROOT/support/sdk/java CLASSPATH
 
 export TOSROOT
 export TOSDIR
 export CLASSPATH
 export MAKERULES
+
+path::addpath /stow/bin
+path::addpath /stow/sbin
+path::addpath /stow/man MANPATH
