53d7583e3317f4cac0246f9d1110928d95911459
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2010-02-27 05:32:30 +0900

    add config.sh

--- nesc-1.3.1.sh
+++ nesc-1.3.1.sh
@@ -1,25 +1,18 @@
 #!/bin/sh -xu
 
 scriptdir=$(dirname $0)
-prefix=/stow
-urlnesc=http://softlayer.dl.sourceforge.net/project/nescc/nescc/v1.3.1
-builddir=nesc-1.3.1
+. $scriptdir/config.sh
 
-expr $(uname -r) : 10.0 > /dev/null && osx10=yes || osx=no
+builddir=build-nesc
 
-function die() {
-    echo "$@" 1>&2
-    exit 1
-}
-
-[ -f nesc-1.3.1.tar.gz ] \
-    || curl -O $urlnesc/nesc-1.3.1.tar.gz \
+[ -f $nesc.tar.gz ] \
+    || curl -O $urlnesc/$nesc.tar.gz \
     || die "can not fetch tarball"
 
 rm -rf $builddir
-tar xzf nesc-1.3.1.tar.gz
-[ "$osx10" = yes ] \
-    && patch -d $builddir -p1 < $scriptdir/nesc-1.3.1-osx_10.patch \
+tar xzf $nesc.tar.gz
+is_osx10 \
+    && patch -d $builddir -p1 < $scriptdir/$nesc-osx_10.patch \
     || die "apply patch failed"
 
 cd $builddir
