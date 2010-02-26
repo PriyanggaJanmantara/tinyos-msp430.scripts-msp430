4c2c8bafd5e7d3f6d9ec00bd321b605df6f41350
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2010-02-27 05:30:47 +0900

    reinforce error check

--- gcc-3.2.3.sh
+++ gcc-3.2.3.sh
@@ -11,10 +11,17 @@ function die() {
     exit 1
 }
 
-[ -f $scriptdir/gcc-3.2.3-config_gcc.patch ] || die $scriptdir/gcc-3.2.3-config_gcc.patch missing
-[ -d mspgcc-gcc ] || cvs -d $cvsroot co -d mspgcc-gcc -P gcc
-[ -f gcc-core-3.2.3.tar.bz2 ] || curl -O $urlbase/gcc-3.2.3/gcc-core-3.2.3.tar.bz2
-[ -f gcc-core-3.3.6.tar.bz2 ] || curl -O $urlbase/gcc-3.3.6/gcc-core-3.3.6.tar.bz2
+[ -f $scriptdir/gcc-3.2.3-config_gcc.patch ] \
+    || die $scriptdir/gcc-3.2.3-config_gcc.patch missing
+[ -d mspgcc-gcc ] \
+    || cvs -d $cvsroot co -d mspgcc-gcc -P gcc \
+    || die "can not fetch from cvs repository"
+[ -f gcc-core-3.2.3.tar.bz2 ] \
+    || curl -O $urlbase/gcc-3.2.3/gcc-core-3.2.3.tar.bz2 \
+    || die "can not fetch tarball"
+[ -f gcc-core-3.3.6.tar.bz2 ] \
+    || curl -O $urlbase/gcc-3.3.6/gcc-core-3.3.6.tar.bz2 \
+    || die "can not fetch tarball"
 
 rm -rf gcc-3.2.3
 tar xjf gcc-core-3.2.3.tar.bz2
@@ -24,12 +31,15 @@ tar cf - -C mspgcc-gcc/gcc-3.3 --exclude=CVS --exclude='THIS*' --exclude='*.patc
     tar xvf - -C gcc-3.2.3
 (cd gcc-3.3.6; tar cf - $(find . -name '*darwin*' -o -name config.gcc)) | \
     tar xvf - -C gcc-3.2.3
-patch -p1 -d gcc-3.2.3 < $scriptdir/gcc-3.2.3-config_gcc.patch
+patch -p1 -d gcc-3.2.3 < $scriptdir/gcc-3.2.3-config_gcc.patch \
+    || die "apply patch failed"
 
 rm -rf $builddir
 mkdir -p $builddir
 cd $builddir
 PATH=$prefix/bin:$PATH
-../gcc-3.2.3/configure --target=msp430 --prefix=$prefix --disable-nls --with-as=$prefix/bin/msp430-as --with-ld=$prefix/bin/msp430-ld
+../gcc-3.2.3/configure --target=msp430 --prefix=$prefix --disable-nls \
+    --with-as=$prefix/bin/msp430-as --with-ld=$prefix/bin/msp430-ld \
+    || die "configure failed"
 make -j32
 # sudo make install
