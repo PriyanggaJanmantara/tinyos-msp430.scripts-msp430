7eb0b444d4b7791195b80d525a7378ab65afaa1f
Tadashi G. Takaoka <takaoka@google.com>
2011-03-25 21:58:47 +0900

    Fix dis430.sh to be able to handle "." as part of label

--- dis430.sh
+++ dis430.sh
@@ -34,21 +34,21 @@
 replace_abssymbol=($(
 	msp430-objdump -t "$@" | \
 	    awk '$2~/[lg]/ && $3=="*ABS*" && $NF!~/^\./ {print $1,$NF}' | \
-	    sed -E -e 's/^0000([0-9a-f]+) ([a-zA-Z0-9_]+)/-e s;((mov|jmp|call|and|bis|bic).*)([#\&])0x\1;\\1\\3\2;/'
+	    sed -E -e 's/^0000([0-9a-f]+) ([a-zA-Z0-9_.]+)/-e s;((mov|jmp|call|and|bis|bic).*)([#\&])0x\1;\\1\\3\2;/'
 	))
 
 # create sed commands to replace absolute labels.
 replace_abslabel=($(
 	msp430-objdump -t "$@" | \
 	    awk '$2~/[lg]/ && $4~/.text|.data|.bss/ && $NF!~/^\./ {print $1,$NF}' | \
-	    sed -E -e 's/^0000([0-9a-f]+) ([a-zA-Z0-9_]+)/-e s;([#\&]?)0x\1;\\1\2;/'
+	    sed -E -e 's/^0000([0-9a-f]+) ([a-zA-Z0-9_.]+)/-e s;([#\&]?)0x\1;\\1\2;/'
 	))
 
 # create sed commands to replace weak symbols.
 replace_weaksymbol=($(
 	msp430-objdump -t "$@" | \
 	    awk '$2~/[wg]/ && $3==".text" && $NF!~/^\./ {print $1,$NF}' | \
-	    sed -E -e 's/^0000([0-9a-f]+) ([a-zA-Z0-9_]+)/-e s;([#\&]?)0x\1;\\1\2;/'
+	    sed -E -e 's/^0000([0-9a-f]+) ([a-zA-Z0-9_.]+)/-e s;([#\&]?)0x\1;\\1\2;/'
 	))
 
 # create sed commands to mark relative jumps operand to its absolute value surrounded by '@'.
@@ -98,12 +98,12 @@ msp430-objdump -d "$@" | \
         "${replace_abssymbol[@]}" \
         "${replace_abslabel[@]}" \
         "${replace_weaksymbol[@]}" \
-        -e 's/^([0-9a-f]{8}) <([a-zA-Z0-9_]+)>:/\2:/' \
+        -e 's/^([0-9a-f]{8}) <([a-zA-Z0-9_.]+)>:/\2:/' \
         "${format_address}" \
         -e 's/( *;)abs 0x[0-9a-f]+/\1/' \
         -e 's/r[23] As==[01][01],?//' \
-        -e 's/([^a-zA-Z0-9_])r1([^a-zA-Z0-9])/\1sp\2/g' \
-        -e 's/([^a-zA-Z0-9_])r2([^a-zA-Z0-9])/\1sr\2/g' \
+        -e 's/([^a-zA-Z0-9_.])r1([^a-zA-Z0-9.])/\1sp\2/g' \
+        -e 's/([^a-zA-Z0-9_.])r2([^a-zA-Z0-9.])/\1sr\2/g' \
         -e 's/ *; *$//'
 
 
