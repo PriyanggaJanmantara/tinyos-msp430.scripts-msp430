6e195455b890efd9e6d2910007bb37fb46ca4056
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2012-08-07 03:00:15 +0900

    Support 20-bit address dissassembler

--- dis430.sh
+++ dis430.sh
@@ -34,21 +34,21 @@
 replace_abssymbol=($(
 	msp430-objdump -t "$@" | \
 	    awk '$2~/[lg]/ && $3=="*ABS*" && $NF!~/^\./ {print $1,$NF}' | \
-	    sed -E -e 's/^0000([0-9a-f]+) ([a-zA-Z0-9_.]+)/-e s;((mov|jmp|call|and|bis|bic).*)([#\&])0x\1;\\1\\3\2;/'
+	    sed -E -e 's/^000([0-9a-f]+) ([a-zA-Z0-9_.]+)/-e s;((mov|jmp|call|and|bis|bic).*)([#\&])0x\1;\\1\\3\2;/'
 	))
 
 # create sed commands to replace absolute labels.
 replace_abslabel=($(
 	msp430-objdump -t "$@" | \
 	    awk '$2~/[lg]/ && $4~/.text|.data|.bss/ && $NF!~/^\./ {print $1,$NF}' | \
-	    sed -E -e 's/^0000([0-9a-f]+) ([a-zA-Z0-9_.]+)/-e s;([#\&]?)0x\1;\\1\2;/'
+	    sed -E -e 's/^000([0-9a-f]+) ([a-zA-Z0-9_.]+)/-e s;([#\&]?)0x\1;\\1\2;/'
 	))
 
 # create sed commands to replace weak symbols.
 replace_weaksymbol=($(
 	msp430-objdump -t "$@" | \
 	    awk '$2~/[wg]/ && $3==".text" && $NF!~/^\./ {print $1,$NF}' | \
-	    sed -E -e 's/^0000([0-9a-f]+) ([a-zA-Z0-9_.]+)/-e s;([#\&]?)0x\1;\\1\2;/'
+	    sed -E -e 's/^000([0-9a-f]+) ([a-zA-Z0-9_.]+)/-e s;([#\&]?)0x\1;\\1\2;/'
 	))
 
 # create sed commands to mark relative jumps operand to its absolute value surrounded by '@'.
